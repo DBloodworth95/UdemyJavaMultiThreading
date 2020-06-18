da2e78f9cf736d54da22c1152f717e096c68128b
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-06-18 13:34:12 +0100

    Section 2, Part 1 - Creating a simple thread.

---
+++ src/main/java/Task01/BasicThreadCreation.java
@@ -0,0 +1,24 @@
+package Task01;
+
+public class BasicThreadCreation {
+    //Example of a basic thread.
+    public static void main(String[] args) {
+        //Declaring the thread which takes a Runnable() as an arg.
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //This code will run in the thread we have created.
+                System.out.println("Thread: " + Thread.currentThread().getName() + " is now running.");
+                System.out.println(Thread.currentThread().getName() + " has a priority of " + Thread.currentThread().getPriority());
+            }
+        });
+        //Setting name of thread.
+        thread.setName("Worker Thread");
+        //Setting the priority (10 being the max, 1 being the lowest).
+        thread.setPriority(Thread.MAX_PRIORITY);
+        System.out.println("Thread: " + Thread.currentThread().getName() + " is about to start.");
+        //Starting the thread.
+        thread.start();
+        System.out.println("Thread: " + Thread.currentThread().getName() + " has started.");
+    }
+}
